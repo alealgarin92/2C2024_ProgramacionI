1485c990c7a3fd9b5f1fa6a6833bedab180bd330
ale4287 <alejandro.algarin@hotmail.com>
2024-11-29 03:12:11 -0300

    Clase 11 UI

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,46 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button playButton, optionsButton, quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(OnPlayButtonPressed);
+        quitButton.onClick.AddListener(OnOptionsButtonPressed);
+        optionsButton.onClick.AddListener(OnQuitButtonPressed);
+    }
+
+    // Update is called once per frame
+    private void OnPlayButtonPressed()
+    {
+        Debug.Log("Play Game");
+    }
+
+    private void OnOptionsButtonPressed()
+    {
+        Debug.Log("Quit Game");
+
+        //Application.Quit(); //Solo funciona en la build, no en el editor
+
+#if UNITY_EDITOR 
+        EditorApplication.ExitPlaymode(); //Solo editor, no funciona en la build!!!
+#endif
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        Debug.Log("Options");
+    }
+
+
+
+
+}
+
